9c0abc21c09379750dcc0d28ae9b10a3bec1204b
hmones <haythameyd@gmail.com>
2020-10-07 15:52:26 +0200

    Added search page for the website

--- plugins/hmones/website/Plugin.php
+++ plugins/hmones/website/Plugin.php
@@ -2,6 +2,7 @@
 
 use Backend;
 use System\Classes\PluginBase;
+use Hmones\Website\Models\Topic;
 
 /**
  * website Plugin Information File
@@ -41,6 +42,46 @@ class Plugin extends PluginBase
     public function boot()
     {
 
+        \Event::listen('offline.sitesearch.query', function ($query) {
+
+            // The controller is used to generate page URLs.
+            $controller = \Cms\Classes\Controller::getController() ?? new \Cms\Classes\Controller();
+
+            // Search your plugin's contents
+            $items = Topic::where('title', 'like', "%${query}%")->orWhere('content', 'like', "%${query}%")->get();
+
+            // Now build a results array
+            $results = $items->map(function ($item) use ($query, $controller) {
+
+                // If the query is found in the title, set a relevance of 2
+                $relevance = mb_stripos($item->title, $query) !== false ? 2 : 1;
+                
+                // Optional: Add an age penalty to older results. This makes sure that
+                // newer results are listed first.
+                // if ($relevance > 1 && $item->created_at) {
+                //    $ageInDays = $item->created_at->diffInDays(\Illuminate\Support\Carbon::now());
+                //    $relevance -= \OFFLINE\SiteSearch\Classes\Providers\ResultsProvider::agePenaltyForDays($ageInDays);
+                // }
+
+                return [
+                    'title'     => $item->title,
+                    'text'      => $item->content,
+                    'url'       => $controller->pageUrl('topic', ['slug' => $item->slug]),
+                    // 'thumb'     => optional($item->image)->first(), // Instance of System\Models\File
+                    'relevance' => $relevance, // higher relevance results in a higher
+                                            // position in the results listing
+                    // 'meta' => 'data',       // optional, any other information you want
+                                            // to associate with this result
+                    // 'model' => $item,       // optional, pass along the original model
+                ];
+            });
+
+            return [
+                'provider' => 'Topic', // The badge to display for this result
+                'results'  => $results,
+            ];
+        });
+    
     }
 
     /**
