2fcc867e8229c5145662cb3caab8435861799419
hmones <haythameyd@gmail.com>
2020-10-09 01:28:04 +0200

    Fixed migration for creating topics table in the database

---
+++ plugins/hmones/website/updates/builder_table_create_hmones_website_topics.php
@@ -0,0 +1,29 @@
+<?php namespace Hmones\Website\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateHmonesWebsiteTopics extends Migration
+{
+    public function up()
+    {
+        Schema::create('hmones_website_topics', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id')->unsigned();
+            $table->text('title');
+            $table->text('content')->nullable();
+            $table->text('slug')->nullable()->index();
+            $table->text('img_credits')->nullable();
+            $table->smallInteger('category')->default(0)->index();
+            $table->timestamp('published_at')->nullable()->index();
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('hmones_website_topics');
+    }
+}
