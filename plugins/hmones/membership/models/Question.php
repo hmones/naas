e524bf4b7c175f35101bb05c0243761183179ac5
Haytham Mones <haytham.mones@easybell.de>
2021-07-02 14:45:05 +0200

    Adjusted conditions on updating order

--- plugins/hmones/membership/models/Question.php
+++ plugins/hmones/membership/models/Question.php
@@ -2,89 +2,19 @@
 
 use Model;
 use DB;
+use October\Rain\Database\Traits\Validation;
+use October\Rain\Database\Traits\SoftDelete;
 
-/**
- * Model
- *
- * @property string|null $add_css
- * @property string|null $add_script
- * @property int|null $cond_option_id
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $deleted_at
- * @property string|null $description
- * @property int $display_order
- * @property int $file_multiple
- * @property string $file_type
- * @property int|null $group
- * @property string|null $header
- * @property int $id
- * @property int $new_member
- * @property int $old_member
- * @property int $other_active
- * @property string|null $other_text
- * @property int $prefill
- * @property int $published
- * @property string $question
- * @property int $repeat_active
- * @property string|null $repeat_text
- * @property int $required
- * @property string|null $subheader
- * @property int|null $theme_id
- * @property int $type
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property string|null $validation
- * @property-read mixed $question_type
- * @method static \October\Rain\Database\Builder|\Hmones\Membership\Models\Question newModelQuery()
- * @method static \October\Rain\Database\Builder|\Hmones\Membership\Models\Question newQuery()
- * @method static \October\Rain\Database\Builder|\Hmones\Membership\Models\Question query()
- * @method static \Illuminate\Database\Eloquent\Builder|\Hmones\Membership\Models\Question whereAddCss($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Hmones\Membership\Models\Question whereAddScript($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Hmones\Membership\Models\Question whereCondOptionId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Hmones\Membership\Models\Question whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Hmones\Membership\Models\Question whereDeletedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Hmones\Membership\Models\Question whereDescription($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Hmones\Membership\Models\Question whereDisplayOrder($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Hmones\Membership\Models\Question whereFileMultiple($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Hmones\Membership\Models\Question whereFileType($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Hmones\Membership\Models\Question whereGroup($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Hmones\Membership\Models\Question whereHeader($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Hmones\Membership\Models\Question whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Hmones\Membership\Models\Question whereNewMember($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Hmones\Membership\Models\Question whereOldMember($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Hmones\Membership\Models\Question whereOtherActive($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Hmones\Membership\Models\Question whereOtherText($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Hmones\Membership\Models\Question wherePrefill($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Hmones\Membership\Models\Question wherePublished($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Hmones\Membership\Models\Question whereQuestion($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Hmones\Membership\Models\Question whereRepeatActive($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Hmones\Membership\Models\Question whereRepeatText($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Hmones\Membership\Models\Question whereRequired($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Hmones\Membership\Models\Question whereSubheader($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Hmones\Membership\Models\Question whereThemeId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Hmones\Membership\Models\Question whereType($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Hmones\Membership\Models\Question whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Hmones\Membership\Models\Question whereValidation($value)
- * @mixin \Eloquent
- */
 class Question extends Model
 {
-    use \October\Rain\Database\Traits\Validation;
-    
-    use \October\Rain\Database\Traits\SoftDelete;
+    use Validation;
+    use SoftDelete;
 
     protected $dates = ['deleted_at'];
 
-
-    /**
-     * @var string The database table used by the model.
-     */
     public $table = 'hmones_membership_questions';
 
-    /**
-     * @var array Validation rules
-     */
-    public $rules = [
-    ];
+    public $rules = [];
 
     public $implement = ['RainLab.Translate.Behaviors.TranslatableModel'];
 
@@ -148,18 +78,18 @@ class Question extends Model
         );
     }
     public function beforeUpdate()
-    {   
+    {
         $oldOrder = intval($this->original['display_order']);
         $newOrder = intval($this->display_order);
 
         if($newOrder > $oldOrder){
-            Question::whereBetween('display_order',[$oldOrder,$newOrder])->update(
+            Question::where('display_order', '>', $oldOrder)->where('display_order', '<=', $newOrder)->update(
                 ['display_order' => DB::raw('display_order - 1')]
-        );
+            );
         }
         if($newOrder < $oldOrder)
         {
-            Question::whereBetween('display_order',[$newOrder,$oldOrder])->update(
+            Question::where('display_order', '>=', $newOrder)->where('display_order', '<', $oldOrder)->update(
                     ['display_order' => DB::raw('display_order + 1')]
             );
         }
